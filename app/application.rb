d12ba2302b8732898c0aa1eaa01c315edf392f7c
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-29 17:39:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,12 +5,11 @@ class Application
     req = Rack::Request.new(env)
 
   if req.path.match(/items/)
-
-    if @@items.include?(Item.name)
-      resp.write Item.price
-    else
-      resp.write "Item not found"
-      resp.status = 400
+    item_name = req.path.split("/items").last
+    resp.write Item.price
+  else
+    resp.write "Item not found"
+    resp.status = 400
     end
   else
       resp.write "Route not found"
