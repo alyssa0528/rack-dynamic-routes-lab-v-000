8522bac5a0af2763a1b487e573f6a5385803fbb2
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-29 17:42:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,11 +6,11 @@ class Application
 
   if req.path.match(/items/)
     item_name = req.path.split("/items").last
-    item = @@item.find{|i| i.name == item_name}
-    resp.write item.price
-  else
-    resp.write "Item not found"
-    resp.status = 400
+    if item = @@item.find{|i| i.name == item_name}
+      resp.write item.price
+    else
+      resp.write "Item not found"
+      resp.status = 400
     end
   else
       resp.write "Route not found"
